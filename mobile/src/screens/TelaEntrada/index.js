4015d8cd90c0fcd619d108e39ea5572e446f854d
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-11-18 11:27:53 -0300

    Continuidade do desenvolvimento do consumo da API pela aplicação mobile.

--- mobile/src/screens/TelaEntrada/index.js
+++ mobile/src/screens/TelaEntrada/index.js
@@ -8,7 +8,9 @@ import { AntDesign, EvilIcons } from '@expo/vector-icons';
 
 import logoloja from '../images/fire.png'; 
 
-import api from '../../services/api';
+import { api, login } from '../../services/api';
+
+const axios = require('axios');
 
 import { 
     BotaoCadastrar, BotaoEntrar,
@@ -21,6 +23,8 @@ import {
     Senha,
 
 } from './styles';
+
+import styles from './styles';
 
 
 export default function Entrada({navigation}) {
@@ -36,6 +40,48 @@ export default function Entrada({navigation}) {
 
     const senhaHandler = ( value ) => {
         setSenha( value );
+    }
+
+    const login = async () => {
+        if ( email && senha ){
+            // let params = {
+            //     email,
+            //     senha
+            // }
+
+            // login( email, senha );
+
+            // try {
+            //     response = await api.get('/sessao/cliente', {
+            //         params: {
+            //             email,
+            //             senha
+            //         }
+            //     });
+                
+            //     console.log(response);
+            // } catch (error) {
+            //     console.log(error);
+            // };
+
+            console.log("Email: " + email + "\nSenha: " + senha );
+
+            let params = {
+                email,
+                senha
+            }
+
+            console.log(params);
+
+            const response = await axios.post('http://192.168.0.18:3333/sessao/cliente',
+                params
+            );
+
+            console.log(response);
+        }
+        else {
+            alert("Informe os dados nos campos.");
+        }
     }
 
     // let [fontLoaded] = useFonts({
@@ -62,18 +108,18 @@ export default function Entrada({navigation}) {
                 <ContainerInput>
                     <Usuario>
                         <AntDesign name="user" size={24} color="black" />
-                        <TextInput placeholder="Digite seu email" onChange={ ( valor ) => emailHandler( valor )}></TextInput>
+                        <TextInput placeholder="Digite seu email" onChangeText={ valor => emailHandler( valor )}></TextInput>
                     </Usuario>
 
                     <Senha>
                         <EvilIcons name="lock" size={35} color="black" />
-                        <TextInput placeholder="Digite seu email"></TextInput>
+                        <TextInput placeholder="Digite seu email"  onChangeText={ valor => senhaHandler( valor )}></TextInput>
                     </Senha>
                 </ContainerInput>
 
                 
                 <ContainerBotao>
-                    <BotaoEntrar><Text style={{fontSize: 30, color: "#FFF"}}>Entrar</Text></BotaoEntrar>
+                    <BotaoEntrar onPress={ () => login() }><Text style={{fontSize: 30, color: "#FFF"}}>Entrar</Text></BotaoEntrar>
                     <BotaoCadastrar><Text style={{fontSize: 30, color: "#FFF"}}>Criar Conta</Text></BotaoCadastrar>
                 </ContainerBotao>
                 <Footer>
--- mobile/src/services/api/index.js
+++ mobile/src/services/api/index.js
@@ -1,9 +1,23 @@
-import axios from 'axios';
+const axios = require('axios');
 
 export const api = axios.create({
     baseUrl: "http://192.168.0.18:3333/"
 });
 
-export const teste = async () => {
-    console.log( await axios.get('/sexos_prestadores_servicos') );
+export async function login ( email, senha ) {
+    try {
+        const response = await api.get('/sessao/cliente', {
+            params: {
+                email,
+                senha
+            }
+        });
+        
+        console.log(response);
+
+        return response
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
